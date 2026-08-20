5b663ad8cc2a62de40e2d7e463c8c8c2d0a1a4ef
Elluri Dhanush Chandra <dhanushelluri62@gmail.com>
2026-08-20 15:34:01 +0530

    Update api.js

--- web/frontend/src/api.js
+++ web/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://career-navigator-2cwd.onrender.com',
 });
 
 // Request interceptor to add the JWT token to headers
